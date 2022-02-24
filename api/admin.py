a9991625f0aba313bf295370dbaf98f22134d666
Freedom Loisa <kitakayaloisa@gmail.com>
2022-02-24 06:51:59 +0300

    created api model, registered it in admin site, created dumy view and url

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import TodoList
 
 # Register your models here.
+class TodoListAdmin(admin.ModelAdmin):
+
+    list_display = ('owner', 'thing_to_do', 'completed', 'created_on')
+    list_filter = ('owner', 'completed', 'created_on')
+
+admin.site.register(TodoList, TodoListAdmin)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class TodoList(models.Model):
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='list_owner')
+    thing_to_do = models.CharField(max_length=250, blank=True)
+    completed = models.BooleanField(default=False)
+    created_on = models.DateField(auto_now_add=True, blank=True)
+
+    class Meta:
+
+        ordering = ['-created_on']
+
+    def __str__(self):
+
+        return self.thing_to_do
