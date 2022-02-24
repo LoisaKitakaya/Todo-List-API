6b2b0a908b676f0131b632387c9f8b87624c2dc7
Freedom Loisa <kitakayaloisa@gmail.com>
2022-02-24 10:50:09 +0300

    configured api serializer and model

--- api/models.py
+++ api/models.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 class TodoList(models.Model):
 
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='list_owner')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='todo_list_owner', blank=True)
     thing_to_do = models.CharField(max_length=250, blank=True)
-    completed = models.BooleanField(default=False)
+    completed = models.BooleanField(default=False, blank=True)
     created_on = models.DateField(auto_now_add=True, blank=True)
 
     class Meta:
--- api/serializers.py
+++ api/serializers.py
@@ -10,6 +10,7 @@ class TodoListSerializer(serializers.ModelSerializer):
     class Meta:
 
         model = TodoList
+
         fields = '__all__'
 
 class UserSerializer(serializers.ModelSerializer):
@@ -17,4 +18,24 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
 
         model = User
-        fields = ['username', 'email', 'password']
+
+        fields = ['username', 'email', 'first_name', 'last_name', 'password']
+
+        extra_kwargs = {
+            'first_name': {'required': True},
+            'last_name': {'required': True}
+        }
+
+    def create(self, validated_data):
+
+        user = User.objects.create(
+            username = validated_data['username'],
+            email = validated_data['email'],
+            first_name = validated_data['first_name'],
+            last_name = validated_data['last_name']
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
